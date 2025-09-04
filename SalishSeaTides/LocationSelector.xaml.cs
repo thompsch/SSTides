63d4f98f565b5249a6ea1b443049301b0305f001
Caleb Thompson <caleb.thompson@mongodb.com>
2025-09-04 16:35:43 -0700

    using gesture map. need to add all stations

--- SalishSeaTides/LocationSelector.xaml.cs
+++ SalishSeaTides/LocationSelector.xaml.cs
@@ -20,4 +20,16 @@ public partial class LocationSelector : ContentPage
         await Navigation.PopModalAsync();
 
     }
+
+    private void TapGestureRecognizer_OnTapped(object? sender, TappedEventArgs e)
+    {
+        Point? position = e.GetPosition((Image)sender);
+        if (position.HasValue)
+        {
+            double x = position.Value.X;
+            double y = position.Value.Y;
+            // Now you can use the (x, y) coordinates to trigger an action.
+            // For example, display an alert or navigate.
+        }
+    }
 }
